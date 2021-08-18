6cc8e22667bd4a0a6c95bfe3b5040e16b5cccb76
psmolenski <pwl.smolenski@gmail.com>
2021-08-18 23:49:01 +0200

    db schema

---
+++ database.sql
@@ -0,0 +1,27 @@
+USE master
+GO
+
+DROP DATABASE IF EXISTS lato2021magazyn
+GO
+
+CREATE DATABASE lato2021magazyn
+GO
+
+USE lato2021magazyn
+GO
+
+CREATE TABLE Produkty (
+    Id INT PRIMARY KEY,
+    Nazwa VARCHAR(50) NOT NULL,
+    Kategoria VARCHAR(50) NOT NULL CHECK (Kategoria IN ('Sport', 'Zabawki', 'Ubrania')),
+    Ilosc SMALLINT NOT NULL CHECK (Ilosc > 0),
+    Cena MONEY NOT NULL CHECK (Cena > 0)
+)
+
+INSERT INTO Produkty VALUES
+(1, 'Rower', 'Sport', 10, 1000),
+(2, 'Pluszowy miś', 'Zabawki', 40, 50),
+(3, 'Koń na biegunach', 'Zabawki', 2, 200),
+(4, 'Spodnie', 'Ubrania', 16, 99.99),
+(5, 'Sandały', 'Ubrania', 21, 78.55),
+(6, 'Rakieta tenisowa', 'Sport', 3, 250)
